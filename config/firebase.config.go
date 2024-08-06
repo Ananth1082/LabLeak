ceb24ed48e67aed75af7755b714eb2af0bbddd82
Ananth1082 <ananthshetty203@gmail.com>
2024-08-06 06:54:08 +0530

    created seperate branch for dev server

--- config/firebase.config.go
+++ config/firebase.config.go
@@ -18,7 +18,7 @@ var Firebase *firebaseClient = new(firebaseClient)
 
 func init() {
 	var err error
-	opt := option.WithCredentialsFile("../lab-manual-9dcc3-firebase-adminsdk-novaw-4b87b72bc0.json")
+	opt := option.WithCredentialsFile("../env/lab-manual-9dcc3-firebase-adminsdk-novaw-4b87b72bc0.json")
 	Firebase.app, err = firebase.NewApp(context.Background(), nil, opt)
 	if err != nil {
 		panic("Error initializing app")
